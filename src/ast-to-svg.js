baee402b8e999418f1e03e3dd0fde9ce21abb418
Trevor Behlman <tsbehlman@gmail.com>
2021-09-30 19:25:45 -0400

    Minor refactoring

--- src/ast-to-svg.js
+++ src/ast-to-svg.js
@@ -2,16 +2,14 @@ export default function process(ast, options) {
     let contents = [];
     options.plugins.forEach(plugin => {
         const nodes = plugin(ast, options);
-        if (!Array.isArray(nodes)) {
-            nodes.push(node);
-            return;
+        if (Array.isArray(nodes)) {
+            contents = contents.concat(nodes);
         }
-        nodes.forEach(node => {
-            contents.push(node);
-        });
     });
     const viewWidth = options.width;
-    const viewHeight = options.height === "auto" ? ast.loc.end.line * options.fontSize + options.fontSize : options.height;
+    const viewHeight = options.height === "auto"
+        ? ( ast.loc.end.line + 1 ) * options.fontSize
+        : options.height;
     return `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${viewWidth} ${viewHeight}">
     <style>${options.css}</style>
     <g id="jscode-to-svg-container" font-family="${options.fontFamily}" font-size="${options.fontSize}">
