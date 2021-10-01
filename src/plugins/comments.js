948669190a346caf17c20c62246db3c070e4ccc1
Trevor Behlman <tsbehlman@gmail.com>
2021-10-01 02:00:49 -0400

    Support multiline comments

--- src/plugins/comments.js
+++ src/plugins/comments.js
@@ -1,26 +1,32 @@
 // LICENSE : MIT
 "use strict";
 import groupBy from "../utils/groupby.js";
+import splitMultilineTokens from "../utils/splitMultilineTokens.js";
+
+function addDelimitersToComments(comments) {
+    return comments.map(comment => ({
+        ...comment,
+        value: comment.type === "Block"
+            ? `/*${comment.value}*/`
+            : `//${comment.value}`
+    }));
+}
+
 export default function commentsTo(ast, options) {
-    const commentsByLine = groupBy(ast.comments, (comment) => {
+    const commentsByLine = groupBy(splitMultilineTokens(addDelimitersToComments(ast.comments)), (comment) => {
         return comment.loc.start.line;
     });
     return Object.entries(commentsByLine).map(([ line, comments ]) => {
         const y = comments[0].loc.start.line * options.lineHeight;
         const lineMargin = " ".repeat(comments[0].loc.start.column);
         const tspans = comments.map((comment, index) => {
-            // TODO: support multi line comment
-            if (comment.type !== "Line") {
-                return;
-            }
             const prevToken = comments[index - 1];
             let margin = "";
             if (prevToken) {
                 margin = " ".repeat(token.start - prevToken.end);
             }
-            const value = `//${comment.value}`;
-            return `${margin}<tspan class="Comment ${comment.type}">${value}</tspan>`;
-        }).filter((text) => text != null);
+            return `${margin}<tspan class="Comment ${comment.type}">${comment.value}</tspan>`;
+        });
         return `<text y="${y}">${lineMargin}${tspans.join("")}</text>`;
     });
 }
